91198bb0067419a069922a70e77c1cd9207ad598
LimpidCrypto <konermann@limpidcrypto.com>
2024-12-20 18:18:39 +0000

    add common models

--- .devcontainer/start.sh
+++ .devcontainer/start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 # Install typescript-json-schema
-deno install -g --allow-env --allow-read npm:typescript-json-schema
-export PATH="/home/vscode/.deno/bin:$PATH"
+deno install --allow-env --allow-read npm:typescript-json-schema
